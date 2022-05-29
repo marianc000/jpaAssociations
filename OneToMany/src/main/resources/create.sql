ef681b37bcf5945bd1fb297ce2fced2a66d4c61b
Marian <kalbasa@example.com>
2022-05-29 12:55:44 +0200

    first

--- OneToMany/src/main/resources/create.sql
+++
@@ -1,5 +0,0 @@
-create table AUTHOR (id integer not null, name varchar(255), COUNTRY_ID integer, primary key (id));
-create table COUNTRY (id integer not null, name varchar(255), primary key (id));
-create table POST (id integer not null, name varchar(255), AUTHOR_ID integer, primary key (id));
-alter table AUTHOR add constraint FKhav5e7wkcm2dgwves8e6lwahi foreign key (COUNTRY_ID) references COUNTRY;
-alter table POST add constraint FK4mb5i28v16s80ccdov4n6fgb6 foreign key (AUTHOR_ID) references AUTHOR;
--- OneToMany/src/main/resources/drop.sql
+++
@@ -1,5 +0,0 @@
-alter table AUTHOR drop constraint FKhav5e7wkcm2dgwves8e6lwahi;
-alter table POST drop constraint FK4mb5i28v16s80ccdov4n6fgb6;
-drop table AUTHOR;
-drop table COUNTRY;
-drop table POST;
